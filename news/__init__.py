095f708705d3366ede615fb7490f250b72fde467
guptasahil670@gmail.com <guptasahil670@gmail.com>
2023-11-03 13:18:56 +0530

    Updated RSS links for pythonanywhere whitelist

--- news/__init__.py
+++ news/__init__.py
@@ -2,9 +2,11 @@ from contentAggregator import Site
 
 __sites = [
     Site('The Times of India', 'toi', 'https://timesofindia.indiatimes.com/', 'https://timesofindia.indiatimes.com/rssfeedmostrecent.cms'),
-    Site('The Economic Times', 'et', 'https://economictimes.indiatimes.com/', 'https://economictimes.indiatimes.com/rssfeedsdefault.cms'),
-    Site('The Hindu', 'hindu', 'https://www.thehindu.com/', 'https://www.thehindu.com/news/national/feeder/default.rss'),
+    # Site('The Economic Times', 'et', 'https://economictimes.indiatimes.com/', 'https://economictimes.indiatimes.com/rssfeedsdefault.cms'),
+    Site('The Economic Times', 'et', 'https://economictimes.indiatimes.com/', 'https://feeds.feedburner.com/theeconomictimes'),
+    # Site('The Hindu', 'hindu', 'https://www.thehindu.com/', 'https://www.thehindu.com/news/national/feeder/default.rss'),
+    Site('The Hindu', 'hindu', 'https://www.thehindu.com/', 'https://feeds.feedburner.com/thehindu-home'),
     Site('NDTV', 'ndtv', 'https://www.ndtv.com/', 'https://feeds.feedburner.com/ndtvnews-latest'),
     Site('Hindustan Times', 'ht', 'https://www.hindustantimes.com/', 'https://www.hindustantimes.com/feeds/rss/latest/rssfeed.xml'),
-    Site('India Today', 'it', 'https://www.indiatoday.in/', 'https://www.indiatoday.in/rss/home'),
+    # Site('India Today', 'it', 'https://www.indiatoday.in/', 'https://www.indiatoday.in/rss/home'),
 ]
--- tech/__init__.py
+++ tech/__init__.py
@@ -1,7 +1,10 @@
 from contentAggregator import Site
 
 __sites = [
-    Site('TechCrunch','techcrunch' , 'https://techcrunch.com/', 'https://techcrunch.com/feed/'),
-    Site('The Crazy Programmer','thecrazyprogrammer', 'https://www.thecrazyprogrammer.com/', 'https://www.thecrazyprogrammer.com/feed'),
-    Site('TechRepublic','techrepublic', 'https://www.techrepublic.com/', 'https://www.techrepublic.com/rssfeeds/articles/'),
+    Site('Stack Overflow','stackoverflow', 'https://www.stackoverflow.com/', 'https://feeds.feedburner.com/stack-overflow'),
+    # Site('TechCrunch','techcrunch' , 'https://techcrunch.com/', 'https://techcrunch.com/feed/'),
+    Site('TechCrunch','techcrunch' , 'https://techcrunch.com/', 'https://feeds.feedburner.com/techcrunch'),
+    # Site('The Crazy Programmer','thecrazyprogrammer', 'https://www.thecrazyprogrammer.com/', 'https://www.thecrazyprogrammer.com/feed'),
+    Site('The Crazy Programmer','thecrazyprogrammer', 'https://www.thecrazyprogrammer.com/', 'https://feeds.feedburner.com/thecrazyprogrammer'),
+    # Site('TechRepublic','techrepublic', 'https://www.techrepublic.com/', 'https://www.techrepublic.com/rssfeeds/articles/'),
 ]
